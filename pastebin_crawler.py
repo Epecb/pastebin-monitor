5086e552d8f0661e8f24e866d7b9e5f3f1b7d8c9
Epecb <788135@gmail.com>
2018-06-21 21:03:48 +0300

    autopep8 and fix typo
        Automatically formats Python code to conform to the PEP 8 style
        guide
        autopep8 --in-place --aggressive --aggressive <name.py>
    
    Signed-off-by: Epecb <788135@gmail.com>

--- pastebin_crawler.py
+++ pastebin_crawler.py
@@ -30,119 +30,121 @@ def get_timestamp():
     """"""
     return time.strftime('%Y/%m/%d %H:%M:%S')
 
+
 def all_python_encodings():
-     return ["ascii",
-             "big5",
-             "big5hkscs",
-             "cp037",
-             "cp424",
-             "cp437",
-             "cp500",
-             "cp720",
-             "cp737",
-             "cp775",
-             "cp850",
-             "cp852",
-             "cp855",
-             "cp856",
-             "cp857",
-             "cp858",
-             "cp860",
-             "cp861",
-             "cp862",
-             "cp863",
-             "cp864",
-             "cp865",
-             "cp866",
-             "cp869",
-             "cp874",
-             "cp875",
-             "cp932",
-             "cp949",
-             "cp950",
-             "cp1006",
-             "cp1026",
-             "cp1140",
-             "cp1250",
-             "cp1251",
-             "cp1252",
-             "cp1253",
-             "cp1254",
-             "cp1255",
-             "cp1256",
-             "cp1257",
-             "cp1258",
-             "euc_jp",
-             "euc_jis_2004",
-             "euc_jisx0213",
-             "euc_kr",
-             "gb2312",
-             "gbk",
-             "gb18030",
-             "hz",
-             "iso2022_jp",
-             "iso2022_jp_1",
-             "iso2022_jp_2",
-             "iso2022_jp_2004",
-             "iso2022_jp_3",
-             "iso2022_jp_ext",
-             "iso2022_kr",
-             "latin_1",
-             "iso8859_2",
-             "iso8859_3",
-             "iso8859_4",
-             "iso8859_5",
-             "iso8859_6",
-             "iso8859_7",
-             "iso8859_8",
-             "iso8859_9",
-             "iso8859_10",
-             "iso8859_13",
-             "iso8859_14",
-             "iso8859_15",
-             "iso8859_16",
-             "johab",
-             "koi8_r",
-             "koi8_u",
-             "mac_cyrillic",
-             "mac_greek",
-             "mac_iceland",
-             "mac_latin2",
-             "mac_roman",
-             "mac_turkish",
-             "ptcp154",
-             "shift_jis",
-             "shift_jis_2004",
-             "shift_jisx0213",
-             "utf_32",
-             "utf_32_be",
-             "utf_32_le",
-             "utf_16",
-             "utf_16_be",
-             "utf_16_le",
-             "utf_7",
-             "utf_8",
-             "utf_8_sig"]
+    """"""
+    return ["ascii",
+            "big5",
+            "big5hkscs",
+            "cp037",
+            "cp424",
+            "cp437",
+            "cp500",
+            "cp720",
+            "cp737",
+            "cp775",
+            "cp850",
+            "cp852",
+            "cp855",
+            "cp856",
+            "cp857",
+            "cp858",
+            "cp860",
+            "cp861",
+            "cp862",
+            "cp863",
+            "cp864",
+            "cp865",
+            "cp866",
+            "cp869",
+            "cp874",
+            "cp875",
+            "cp932",
+            "cp949",
+            "cp950",
+            "cp1006",
+            "cp1026",
+            "cp1140",
+            "cp1250",
+            "cp1251",
+            "cp1252",
+            "cp1253",
+            "cp1254",
+            "cp1255",
+            "cp1256",
+            "cp1257",
+            "cp1258",
+            "euc_jp",
+            "euc_jis_2004",
+            "euc_jisx0213",
+            "euc_kr",
+            "gb2312",
+            "gbk",
+            "gb18030",
+            "hz",
+            "iso2022_jp",
+            "iso2022_jp_1",
+            "iso2022_jp_2",
+            "iso2022_jp_2004",
+            "iso2022_jp_3",
+            "iso2022_jp_ext",
+            "iso2022_kr",
+            "latin_1",
+            "iso8859_2",
+            "iso8859_3",
+            "iso8859_4",
+            "iso8859_5",
+            "iso8859_6",
+            "iso8859_7",
+            "iso8859_8",
+            "iso8859_9",
+            "iso8859_10",
+            "iso8859_13",
+            "iso8859_14",
+            "iso8859_15",
+            "iso8859_16",
+            "johab",
+            "koi8_r",
+            "koi8_u",
+            "mac_cyrillic",
+            "mac_greek",
+            "mac_iceland",
+            "mac_latin2",
+            "mac_roman",
+            "mac_turkish",
+            "ptcp154",
+            "shift_jis",
+            "shift_jis_2004",
+            "shift_jisx0213",
+            "utf_32",
+            "utf_32_be",
+            "utf_32_le",
+            "utf_16",
+            "utf_16_be",
+            "utf_16_le",
+            "utf_7",
+            "utf_8",
+            "utf_8_sig"]
 
 
 class Logger:
     """"""
 
     shell_mod = {
-        '':'',
-       'PURPLE' : '\033[95m',
-       'CYAN' : '\033[96m',
-       'DARKCYAN' : '\033[36m',
-       'BLUE' : '\033[94m',
-       'GREEN' : '\033[92m',
-       'YELLOW' : '\033[93m',
-       'RED' : '\033[91m',
-       'BOLD' : '\033[1m',
-       'UNDERLINE' : '\033[4m',
-       'RESET' : '\033[0m'
+        '': '',
+        'PURPLE': '\033[95m',
+        'CYAN': '\033[96m',
+        'DARKCYAN': '\033[36m',
+        'BLUE': '\033[94m',
+        'GREEN': '\033[92m',
+        'YELLOW': '\033[93m',
+        'RED': '\033[91m',
+        'BOLD': '\033[1m',
+        'UNDERLINE': '\033[4m',
+        'RESET': '\033[0m'
     }
 
-    def log ( self, message, is_bold=False, color='', log_time=True):
+    def log(self, message, is_bold=False, color='', log_time=True):
         """"""
         prefix = ''
         suffix = ''
@@ -158,7 +160,7 @@ class Logger:
             suffix = self.shell_mod['RESET']
 
         message = prefix + message + suffix
-        print ( message )
+        print(message)
         sys.stdout.flush()
 
     def error(self, err):
@@ -188,22 +190,25 @@ class Crawler:
     def read_regexes(self):
         """"""""
         try:
-            with open ( self.REGEXES_FILE, 'r') as f:
+            with open(self.REGEXES_FILE, 'r') as regexes_file:
                 try:
-                    self.regexes = [ [ field.strip() for field in line.split(',')] for line in f.readlines() if line.strip() != '' and not line.startswith('#')]
+                    self.regexes = [[field.strip() for field in line.split(
+                        ',')] for line in regexes_file.readlines() if line.strip() != '' and not line.startswith('#')]
 
                     # In case commas exist in the regexes...merge everything.
                     for i in range(len(self.regexes)):
-                        self.regexes[i] = [','.join(self.regexes[i][:-2])] + self.regexes[i][-2:]
+                        self.regexes[i] = ([','.join(self.regexes[i][:-2])] +
+                                           self.regexes[i][-2:])
                 except KeyboardInterrupt:
                     raise
-                except:
-                    Logger().fatal_error('Malformed regexes file. Format: regex_pattern,URL logging file, directory logging file.')
+                except BaseException:
+                    Logger().fatal_error(
+                        'Malformed regexes file. Format: regex_pattern,URL logging file, directory logging file.')
         except KeyboardInterrupt:
             raise
-        except:
-            Logger().fatal_error('{:s} not found or not acessible.'.format(self.REGEXES_FILE))
-
+        except BaseException:
+            Logger().fatal_error('{:s} not found or not acessible.'.
+                                 format(self.REGEXES_FILE))
 
     def __init__(self):
         self.read_regexes()
@@ -216,21 +221,22 @@ class Crawler:
                            headers={'user-agent': get_useragent()})
         except KeyboardInterrupt:
             raise
-        except:
-            return self.CONNECTION_FAIL,None
-
+        except BaseException:
+            return self.CONNECTION_FAIL, None
 
         """
-        There are a set of encoding issues which, coupled with some bugs in etree (such as in the Raspbian packages) can
-        trigger encoding exceptions here. As a workaround, we try every possible encoding first, and even if that fails,
-        we resort to a very hacky workaround whereby we manually get the page and attempt to encode it as utf-8. It's
-        ugly, but it works for now.
+        There are a set of encoding issues which, coupled with some bugs in
+        etree (such as in the Raspbian packages) can trigger encoding
+        exceptions here. As a workaround, we try every possible encoding first,
+        and even if that fails, we resort to a very hacky workaround whereby we
+        manually get the page and attempt to encode it as utf-8. It's ugly, but
+        it works for now.
         """
         try:
-            page_html = page.html ()
+            page_html = page.html()
         except KeyboardInterrupt:
             raise
-        except:
+        except BaseException:
             worked = False
             for enc in all_python_encodings():
                 try:
@@ -239,7 +245,7 @@ class Crawler:
                     break
                 except KeyboardInterrupt:
                     raise
-                except:
+                except BaseException:
                     pass
             if not worked:
                 # One last try...
@@ -249,12 +255,13 @@ class Crawler:
                     page_html = PyQuery(req.text).html()
                 except KeyboardInterrupt:
                     raise
-                except:
+                except BaseException:
                     return self.OTHER_ERROR, None
-        if re.match ( r'Pastebin\.com - Access Denied Warning', page_html, re.IGNORECASE ) or 'blocked your IP' in page_html:
-            return self.ACCESS_DENIED,None
+        if re.match(r'Pastebin\.com - Access Denied Warning', page_html,
+                    re.IGNORECASE) or 'blocked your IP' in page_html:
+            return self.ACCESS_DENIED, None
         else:
-            return self.OK,page('.maintable img').next('a')
+            return self.OK, page('.maintable img').next('a')
 
     def check_paste(self, paste_id):
         paste_url = self.PASTEBIN_URL + "/raw" + paste_id
@@ -272,11 +279,11 @@ class Crawler:
                     self.save_result(paste_url, paste_id, file, directory,
                                      paste_txt)
                     return True
-            Logger ().log ( 'Not matching paste: ' + paste_url )
+            Logger().log('Not matching paste: ' + paste_url)
         except KeyboardInterrupt:
             raise
-        except:
-            Logger ().log ( 'Error reading paste (probably a 404 or encoding issue).', True, 'YELLOW')
+        except BaseException:
+            Logger().log('Error reading paste (probably a 404 or encoding issue).', True, 'YELLOW')
         return False
 
     def save_result(self, paste_url, paste_id, file, directory, paste_txt):
@@ -288,7 +295,7 @@ class Crawler:
             os.makedirs(directory)
         except KeyboardInterrupt:
             raise
-        except:
+        except BaseException:
             pass
 
         with open(file, 'a') as matching:
@@ -307,16 +314,16 @@ class Crawler:
         """"""
         count = 0
         while True:
-            status,pastes = self.get_pastes ()
+            status, pastes = self.get_pastes()
 
             start_time = time.time()
             if status == self.OK:
                 for paste in pastes:
-                    paste_id = PyQuery ( paste ).attr('href')
-                    self.new_checked_ids.append ( paste_id )
+                    paste_id = PyQuery(paste).attr('href')
+                    self.new_checked_ids.append(paste_id)
                     if paste_id not in self.prev_checked_ids:
-                        self.check_paste ( paste_id )
-                        time.sleep ( delay )
+                        self.check_paste(paste_id)
+                        time.sleep(delay)
                     count += 1
 
                 if count == flush_after_x_refreshes:
@@ -327,21 +334,30 @@ class Crawler:
                 self.new_checked_ids = []
 
                 elapsed_time = time.time() - start_time
-                sleep_time = ceil(max(0,(refresh_time - elapsed_time)))
+                sleep_time = ceil(max(0, (refresh_time - elapsed_time)))
                 if sleep_time > 0:
-                    Logger().log('Waiting {:d} seconds to refresh...'.format(sleep_time), True)
-                    time.sleep ( sleep_time )
+                    Logger().log(
+                        'Waiting {:d} seconds to refresh...'.format(sleep_time), True)
+                    time.sleep(sleep_time)
             elif status == self.ACCESS_DENIED:
-                Logger ().log ( 'Damn! It looks like you have been banned (probably temporarily)', True, 'YELLOW' )
-                for n in range ( 0, ban_wait ):
-                    Logger ().log ( 'Please wait ' + str ( ban_wait - n ) + ' minute' + ( 's' if ( ban_wait - n ) > 1 else '' ) )
-                    time.sleep ( 60 )
+                Logger().log(
+                    'Damn! It looks like you have been banned (probably temporarily)',
+                    True,
+                    'YELLOW')
+                for i in range(0, ban_wait):
+                    Logger().log('Please wait ' + str(ban_wait - i) +
+                                 ' minute' +
+                                 ('s' if (ban_wait - i) > 1 else ''))
+                    time.sleep(60)
             elif status == self.CONNECTION_FAIL:
-                Logger().log ( 'Connection down. Waiting {:d} seconds and trying again'.format(connection_timeout), True, 'RED')
+                Logger().log('Connection down. Waiting {:d} seconds and trying again'.format(
+                    connection_timeout), True, 'RED')
                 time.sleep(connection_timeout)
             elif status == self.OTHER_ERROR:
-                Logger().log('Unknown error. Maybe an encoding problem? Trying again.'.format(connection_timeout), True,'RED')
+                Logger().log('Unknown error. Maybe an encoding problem? Trying again in {:d} seconds .'.format(
+                    connection_timeout), True, 'RED')
                 time.sleep(1)
+
 
 def parse_input():
     """"""
@@ -387,6 +403,6 @@ def parse_input():
 
 
 try:
-    Crawler ().start (*parse_input())
+    Crawler().start(*parse_input())
 except KeyboardInterrupt:
-    Logger ().log ( 'Bye! Hope you found what you were looking for :)', True )
+    Logger().log('Bye! Hope you found what you were looking for :)', True)
