22dce4635d606b1b3a060b6eb499141853a5d8d5
Epecb <788135@gmail.com>
2018-06-22 06:36:22 +0300

    Typo
    Signed-off-by: Epecb <788135@gmail.com>

--- pastebin_crawler.py
+++ pastebin_crawler.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-    A simple Pastebin crawler which looks for interesting things and saves
-    them to disk
-"""
+"""A simple Pastebin crawler which looks for interesting things and saves them
+to disk."""
 from math import ceil
 from argparse import ArgumentParser as OptionParser
 import os
@@ -16,13 +14,13 @@ from pyquery import PyQuery
 
 
 def show_paste(paste_txt):
-    """ paste Feed """
+    """paste Feed."""
     Logger().log(message=paste_txt[0:256], is_bold=False, color='YELLOW',
                  log_time=False)
 
 
 def get_useragent():
-    """ Return user-agent """
+    """Return user-agent."""
     return 'Mozilla/5.0 (Windows NT 6.1; rv:52.0) Gecko/20100101 Firefox/52.0'
 
 
@@ -33,98 +31,98 @@ def get_timestamp():
 
 def all_python_encodings():
     """"""
-    return ["ascii",
-            "big5",
-            "big5hkscs",
-            "cp037",
-            "cp424",
-            "cp437",
-            "cp500",
-            "cp720",
-            "cp737",
-            "cp775",
-            "cp850",
-            "cp852",
-            "cp855",
-            "cp856",
-            "cp857",
-            "cp858",
-            "cp860",
-            "cp861",
-            "cp862",
-            "cp863",
-            "cp864",
-            "cp865",
-            "cp866",
-            "cp869",
-            "cp874",
-            "cp875",
-            "cp932",
-            "cp949",
-            "cp950",
-            "cp1006",
-            "cp1026",
-            "cp1140",
-            "cp1250",
-            "cp1251",
-            "cp1252",
-            "cp1253",
-            "cp1254",
-            "cp1255",
-            "cp1256",
-            "cp1257",
-            "cp1258",
-            "euc_jp",
-            "euc_jis_2004",
-            "euc_jisx0213",
-            "euc_kr",
-            "gb2312",
-            "gbk",
-            "gb18030",
-            "hz",
-            "iso2022_jp",
-            "iso2022_jp_1",
-            "iso2022_jp_2",
-            "iso2022_jp_2004",
-            "iso2022_jp_3",
-            "iso2022_jp_ext",
-            "iso2022_kr",
-            "latin_1",
-            "iso8859_2",
-            "iso8859_3",
-            "iso8859_4",
-            "iso8859_5",
-            "iso8859_6",
-            "iso8859_7",
-            "iso8859_8",
-            "iso8859_9",
-            "iso8859_10",
-            "iso8859_13",
-            "iso8859_14",
-            "iso8859_15",
-            "iso8859_16",
-            "johab",
-            "koi8_r",
-            "koi8_u",
-            "mac_cyrillic",
-            "mac_greek",
-            "mac_iceland",
-            "mac_latin2",
-            "mac_roman",
-            "mac_turkish",
-            "ptcp154",
-            "shift_jis",
-            "shift_jis_2004",
-            "shift_jisx0213",
-            "utf_32",
-            "utf_32_be",
-            "utf_32_le",
-            "utf_16",
-            "utf_16_be",
-            "utf_16_le",
-            "utf_7",
-            "utf_8",
-            "utf_8_sig"]
+    return ['ascii',
+            'big5',
+            'big5hkscs',
+            'cp037',
+            'cp424',
+            'cp437',
+            'cp500',
+            'cp720',
+            'cp737',
+            'cp775',
+            'cp850',
+            'cp852',
+            'cp855',
+            'cp856',
+            'cp857',
+            'cp858',
+            'cp860',
+            'cp861',
+            'cp862',
+            'cp863',
+            'cp864',
+            'cp865',
+            'cp866',
+            'cp869',
+            'cp874',
+            'cp875',
+            'cp932',
+            'cp949',
+            'cp950',
+            'cp1006',
+            'cp1026',
+            'cp1140',
+            'cp1250',
+            'cp1251',
+            'cp1252',
+            'cp1253',
+            'cp1254',
+            'cp1255',
+            'cp1256',
+            'cp1257',
+            'cp1258',
+            'euc_jp',
+            'euc_jis_2004',
+            'euc_jisx0213',
+            'euc_kr',
+            'gb2312',
+            'gbk',
+            'gb18030',
+            'hz',
+            'iso2022_jp',
+            'iso2022_jp_1',
+            'iso2022_jp_2',
+            'iso2022_jp_2004',
+            'iso2022_jp_3',
+            'iso2022_jp_ext',
+            'iso2022_kr',
+            'latin_1',
+            'iso8859_2',
+            'iso8859_3',
+            'iso8859_4',
+            'iso8859_5',
+            'iso8859_6',
+            'iso8859_7',
+            'iso8859_8',
+            'iso8859_9',
+            'iso8859_10',
+            'iso8859_13',
+            'iso8859_14',
+            'iso8859_15',
+            'iso8859_16',
+            'johab',
+            'koi8_r',
+            'koi8_u',
+            'mac_cyrillic',
+            'mac_greek',
+            'mac_iceland',
+            'mac_latin2',
+            'mac_roman',
+            'mac_turkish',
+            'ptcp154',
+            'shift_jis',
+            'shift_jis_2004',
+            'shift_jisx0213',
+            'utf_32',
+            'utf_32_be',
+            'utf_32_le',
+            'utf_16',
+            'utf_16_be',
+            'utf_16_le',
+            'utf_7',
+            'utf_8',
+            'utf_8_sig']
 
 
 class Logger:
@@ -188,7 +186,7 @@ class Crawler:
     new_checked_ids = []
 
     def read_regexes(self):
-        """"""""
+        """"""
         try:
             with open(self.REGEXES_FILE, 'r') as regexes_file:
                 try:
@@ -264,7 +262,7 @@ class Crawler:
             return self.OK, page('.maintable img').next('a')
 
     def check_paste(self, paste_id):
-        paste_url = self.PASTEBIN_URL + "/raw" + paste_id
+        paste_url = self.PASTEBIN_URL + '/raw' + paste_id
         try:
             req = requests.get(paste_url,
                                headers={'user-agent': get_useragent()})
